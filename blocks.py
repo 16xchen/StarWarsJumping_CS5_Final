c6a9e69b56c1d7a33a60e9d54327fed36e470169
mnara <c16mn@users.noreply.github.com>
2016-12-06 22:46:41 -0800

    Add files via upload
    Added slowing down

--- blocks.py
+++ blocks.py
@@ -28,18 +28,13 @@ def make_blocks():
     return Blocks
 
 
-def randGeb(Blocks):
-    for i in range(len(Blocks)):
-        Blocks[i].pos = vector(random.randint(-25, 25), 15*(i-3), 0)
-        Blocks[i].vel = vector(0,0,0)
-    return Blocks
-
 def main():
     wall = box(pos=(0,0,-10), length=50, height=90, width = 0.5, color=color.white)
 
     Blocks = make_blocks()
     b1, b2, b3, b4, b5, b6, b7, b8, b9, b10 = Blocks #, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20 = Blocks
 
+    gravity = 9.8
     RATE = 30             # number of loops per second to run, if possible!
     dt = 1.0/(1.0*RATE)   # the amount of time per loop (again, if possible)
     autocenter = True     # do you want vPython to keep the scene centered?
@@ -47,31 +42,49 @@ def main():
     speed_mult = 1
     newpos = (0,0,0)
     #initial positions and velocities
-    Blocks=randGeb(Blocks)
+    for i in range(len(Blocks)):
+        Blocks[i].pos = (random.randint(-25, 25), 15*(i-3), 0)
+        Blocks[i].vel = vector(0,0,0)
+
+    print(type(Blocks[i].vel))
     poslist = []
+
+    print(dt)
+    print(Blocks[i].vel*dt)
     while True:
         rate(RATE)
-        for b in Blocks:
-            poslist += [b.pos.y]
-            if mag(b.vel) > 0: b.vel.y = b.vel.y + 9.8
-        if min(poslist) > -30: L[maxpos(L)].pos = vector(random.randint(-25, 25), -45, 0)
+
+        #physics
+        for i in range(len(Blocks)):
+            Blocks[i].pos = Blocks[i].pos + Blocks[i].vel*dt
+
+
+        for i in range(len(Blocks)):
+            poslist += [Blocks[i].pos.y]
+            if Blocks[i].vel.y < 0: Blocks[i].vel.y = Blocks[i].vel.y + gravity*dt
+
+        if max(poslist) < 90: 
+            Blocks[minpos(Blocks)].pos = vector(random.randint(-25, 25), 105, 0)
+
+        poslist = []
+
         if scene.kb.keys:   # any keypress to be handled?
             s = scene.kb.getkey()
             print "You pressed the key", s
             if s == ' ':
                 for b in Blocks:
-                    b.vel = (0,-10,0)
+                    b.vel = vector(0,-20,0)
 
 
 
-def maxpos(L):
-    maxlist = []
+def minpos(L):
+    minlist = []
     for i in range(len(L)):
-        maxlist += [L[i].pos.y]
+        minlist += [L[i].pos.y]
     for i in range(len(L)):
-        if L[i].pos.y == max(maxlist): return i
+        if L[i].pos.y == min(minlist): return i
 
 # This should be the FINAL thing in the file...
-#if __name__ == "__main__":
- #   main()  # did we just RUN this file?
+if __name__ == "__main__":
+    main()  # did we just RUN this file?
                      # if so, we call main()
